755e98e7ca7acb83c3715f6c0b1f7ac863f7b4ff
mattoverby <mattoverby@gmail.com>
2021-10-19 20:53:22 -0500

    shape matchign

---
+++ scripts/gen_file_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from glob import glob
+
+# Print usage
+if len(sys.argv) < 2:
+	print("Generates a text file of absolute paths of directory contents")
+	print("Ignores .md files")
+	print("Usage: ./gen_file_list.py <directory1> <directory2> ...")
+	print("Output file is: files.txt")
+	sys.exit()
+
+if len(sys.argv) > 3:
+	print("To-do: more than one directory")
+	sys.exit()
+
+# build directory
+listdir = sys.argv[1]
+if listdir[-1] != '/':
+	listdir += '/'
+
+files = [y for x in os.walk(listdir) for y in glob(os.path.join(x[0], '*.*'))]
+files.sort()
+
+outputname = 'files.txt'
+outfile = open(outputname, 'w')
+for f in files:
+	if not os.path.isfile(f):
+		continue
+	if os.path.splitext(f)[1] == '.md':
+		continue
+	fpath = os.path.abspath(f)
+	outfile.write("%s\n" % fpath)
---
+++ shapematching/README.md
@@ -0,0 +1,6 @@
+# Shape Matching
+
+Volumetric meshes with mapped surface
+
+- arm: [GINI Fig. 10, Overby et al. 2021](https://doi.org/10.1111/cgf.14361), original surface mesh from [libigl](https://github.com/libigl/libigl-tutorial-data/blob/master/arm.obj)
+- hand: [GINI Fig. 1, Overby et al. 2021](https://doi.org/10.1111/cgf.14361), original surface mesh from [libigl](https://github.com/libigl/libigl-tutorial-data/blob/master/hand.tgf)
